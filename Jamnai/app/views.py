b378a5490b5a8a0790ba52854fea49937acb6778
sabbir772002 <sabbir772002@gmail.com>
2025-09-21 14:02:17 +0600

    updated analytics

--- Jamnai/app/views.py
+++ Jamnai/app/views.py
@@ -172,7 +172,7 @@ def search_routes(request):
                                         'last_departure': big.departure_time.strftime("%H:%M") if big and big.departure_time else "Not Departed",
                                         'estimated_time': estimated_time if big else "Infinity", 
                                         'updated_at': current_time_bd,
-                                        'seat_avialable':trip.available_seats,
+                                        'seat_available':trip.available_seats,
                                         'total_seat':trip.total_seats,
                                     })
                                 print(buses_info)
